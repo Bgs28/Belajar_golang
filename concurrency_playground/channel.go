6a5ba4e1cbd2bcfd6ee8e025f25864c9812e868c
Katon Bagas <katonb380@gmail.com>
2026-03-05 22:15:05 +0700

    updated learn concurrency go

---
+++ concurrency_playground/channel.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	ch := make(chan string)
+
+// 	go func() {
+// 		ch <- "Hello Channel"
+// 	}()
+
+// 	message := <-ch
+
+// 	fmt.Println(message)
+// }
---
+++ concurrency_playground/goroutine.go
@@ -0,0 +1,17 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func sayHello() {
+// 	fmt.Println("Hello Jarvis")
+// }
+// func main() {
+// 	go sayHello()
+
+// 	fmt.Println("Hello Stark")
+
+// 	time.Sleep(1 * time.Second)
+// }
